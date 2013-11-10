6b344e4341314f7ea3e6c53020e87a3f55ba5402
Brooke Kuhlmann <brooke@redalchemist.com>
2013-11-10 11:45:57 -0700

    Enabled HTTPS support for template file download.

--- template.rb
+++ template.rb
@@ -14,7 +14,6 @@ def download_file source, destination
   uri = URI.parse source
   http = Net::HTTP.new uri.host, uri.port
   http.use_ssl = uri.scheme == "https"
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
   request = Net::HTTP::Get.new uri.request_uri
   response = http.request request
   project_file = File.join destination_root, destination
