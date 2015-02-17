139dadc0a1ff99af7611ed5856dfea141eee3f8e
Brooke Kuhlmann <brooke@alchemists.io>
2015-02-16 19:21:56 -0700

    Added ActiveSupport time helpers to the Rails helper.
    - Enables out-of-the-box support for time helpers (without the need to
      include the Timecop gem).

--- rails/spec/rails_helper.rb
+++ rails/spec/rails_helper.rb
@@ -14,4 +14,5 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.include AbstractController::Translation
+  config.include ActiveSupport::Testing::TimeHelpers
 end
