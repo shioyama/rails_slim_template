6b06055f5a1728facf6a8b2dfe55027568ccc060
Brooke Kuhlmann <brooke@alchemists.io>
2015-02-26 20:22:06 -0700

    Updated VCR default settings.

--- rails/spec/support/extensions/ruby/vcr.rb
+++ rails/spec/support/extensions/ruby/vcr.rb
@@ -1,6 +1,11 @@
 require "vcr"
 
 VCR.configure do |config|
+  config.default_cassette_options = {
+    record: :new_episodes,
+    re_record_interval: 7.days
+  }
   config.cassette_library_dir = "spec/cassettes"
+  config.ignore_localhost = true
   config.configure_rspec_metadata!
 end
