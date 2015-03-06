f4130b2a08b8fd63442ada7d7658b267ff35c2d4
Brooke Kuhlmann <brooke@alchemists.io>
2015-03-05 19:31:12 -0700

    Updated RSpec kit helpers and contexts to use Kit module.
    - Creates a unique namespace for these custom kit helpers and
      contexts.
    - Allows for future expansion with minimal (hopefully) impact to RSpec
      Core code.

--- rails/spec/support/kit/helpers.rb
+++ rails/spec/support/kit/helpers.rb
@@ -1,11 +1,13 @@
 module RSpec
-  module Helpers
-    def also message
-      block_given? ? yield : skip(message)
+  module Kit
+    module Helpers
+      def also message
+        block_given? ? yield : skip(message)
+      end
     end
   end
 end
 
 RSpec.configure do |config|
-  config.include RSpec::Helpers
+  config.include RSpec::Kit::Helpers
 end
