3754d957129b1c7a995da13570149958d37a5d42
Brooke Kuhlmann <brooke@alchemists.io>
2015-04-18 09:38:34 -0600

    Updated RSpec database cleaner configuration.
    - A more intelligent setup with documentation.

--- rails/spec/support/extensions/rails/database_cleaner.rb
+++ rails/spec/support/extensions/rails/database_cleaner.rb
@@ -1,11 +1,17 @@
 RSpec.configure do |config|
   config.before(:suite) { DatabaseCleaner.clean_with :truncation }
+  config.before { DatabaseCleaner.strategy = :transaction }
 
-  config.before do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.start
+  config.before type: :feature do
+    if Capybara.current_driver == :rack_test
+      # The Rack driver shares the app's database connection so the transaction strategy can be used for speed.
+      DatabaseCleaner.strategy = :transaction
+    else
+      # Other drivers (generally JavaScript related) don't share the database connection so truncation is required.
+      DatabaseCleaner.strategy = :truncation
+    end
   end
 
-  config.before(js: true) { DatabaseCleaner.strategy = :truncation }
+  config.before { DatabaseCleaner.start }
   config.after { DatabaseCleaner.clean }
 end
