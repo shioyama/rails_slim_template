3cc262bb746f09210b186936d2c659da5c0b55da
Brooke Kuhlmann <brooke@alchemists.io>
2015-04-18 10:45:59 -0600

    Fixed VCR re-record interval to be seconds.
    - Doesn't assume ActiveSupport is loaded for improve reliability.

--- rails/spec/support/extensions/ruby/vcr.rb
+++ rails/spec/support/extensions/ruby/vcr.rb
@@ -3,7 +3,7 @@ require "vcr"
 VCR.configure do |config|
   config.default_cassette_options = {
     record: :new_episodes,
-    re_record_interval: 7.days
+    re_record_interval: 604_800 # 7 days in seconds (60 * 60 * 24 * 7).
   }
   config.cassette_library_dir = "spec/cassettes"
   config.ignore_localhost = true
