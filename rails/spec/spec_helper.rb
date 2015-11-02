f7bb86cc8d68bd86257bb0e7b82934dfd3b480fc
Brooke Kuhlmann <brooke@alchemists.io>
2015-11-02 10:40:44 -0700

    Removed commented RSpec configuration code.
    - Proved to be more of a distraction than useful.

--- rails/spec/spec_helper.rb
+++ rails/spec/spec_helper.rb
@@ -1,6 +1,2 @@
 Dir[File.join(File.dirname(__FILE__), "support/extensions/ruby/**/*.rb")].each { |file| require file }
 Dir[File.join(File.dirname(__FILE__), "support/kit/**/*.rb")].each { |file| require file }
-
-# Uncomment to add a custom configuration. For the default configuration, see the "support/kit" folder.
-# RSpec.configure do |config|
-# end
