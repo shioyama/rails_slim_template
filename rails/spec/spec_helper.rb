c43a50bd96b4f52b129d87c50429c06418f18e6d
Brooke Kuhlmann <brooke@alchemists.io>
2015-11-02 14:53:40 -0700

    Refactored RSpec Code Climate setup.
    - Necessary to ensure Code Climate is loaded as soon
      as possible for improved analysis.

--- rails/spec/spec_helper.rb
+++ rails/spec/spec_helper.rb
@@ -1,2 +1,7 @@
+if ENV["CI"]
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+end
+
 Dir[File.join(File.dirname(__FILE__), "support/extensions/ruby/**/*.rb")].each { |file| require file }
 Dir[File.join(File.dirname(__FILE__), "support/kit/**/*.rb")].each { |file| require file }
