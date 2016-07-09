40dfa472c19ff5a67612e36884a9390dd0bbbe0f
Brooke Kuhlmann <brooke@alchemists.io>
2016-07-09 13:54:24 -0600

    Removed frozen string literal pragma from templates.
    - These could have been left as is but am opting to remove
      these on purpose so that it can be uniformly managed by
      the Pragmater gem during template generation.

--- rails/spec/spec_helper.rb
+++ rails/spec/spec_helper.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 if ENV["CI"]
   require "codeclimate-test-reporter"
   CodeClimate::TestReporter.start
--- rails/spec/support/shared_contexts/temp_dir.rb
+++ rails/spec/support/shared_contexts/temp_dir.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 RSpec.shared_context "Temporary Directory", :temp_dir do
   let(:temp_dir) { File.expand_path "../../../../tmp/rspec", __FILE__ }
 
