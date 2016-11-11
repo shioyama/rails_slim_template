4ba17294e208adcb7b1aafa5f39424402b4d6c50
Brooke Kuhlmann <brooke@alchemists.io>
2016-11-11 11:22:06 -0700

    Added Reek support.
    - In addition to Rubocop, this provides additional code coverage checks
      (especially code smells).

--- template.rb
+++ template.rb
@@ -114,6 +114,7 @@ create_file "spec/factories.rb"
 
 # Code Quality
 get "#{SLIM_TEMPLATE_ROOT}/rails/lib/tasks/rails_best_practices.rake", "lib/tasks/rails_best_practices.rake"
+get "#{SLIM_TEMPLATE_ROOT}/rails/lib/tasks/reek.rake", "lib/tasks/reek.rake"
 get "#{SLIM_TEMPLATE_ROOT}/rails/lib/tasks/rubocop.rake", "lib/tasks/rubocop.rake"
 get "#{SLIM_TEMPLATE_ROOT}/rails/lib/tasks/scss.rake", "lib/tasks/scss.rake"
 get "#{SLIM_TEMPLATE_ROOT}/rails/codeclimate.yml", ".codeclimate.yml"
