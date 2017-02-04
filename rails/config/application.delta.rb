158969bc81dcff8542f2a4603f5411d66c836085
Brooke Kuhlmann <brooke@alchemists.io>
2017-02-04 12:56:51 -0700

    Removed controller spec generation from Rails application.
    - Removes unnecessary generation of controller specs (use request specs
      instead).

--- rails/config/application.delta.rb
+++ rails/config/application.delta.rb
@@ -2,7 +2,9 @@
     # Override generator defaults
     config.generators do |generator|
       generator.assets false
+      generator.controller_specs false
       generator.helper false
-      generator.view_specs false
       generator.test_framework :rspec
+      generator.view_specs false
     end
+
