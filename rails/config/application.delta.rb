a942e5c58f8b6cd5b6bbfb782dbade9bad8939f0
Brooke Kuhlmann <brooke@alchemists.io>
2017-03-10 11:00:58 -0700

    Updated application generators to default to Slim templates.
    - Use Slim, by default, instead of ERB.

--- rails/config/application.delta.rb
+++ rails/config/application.delta.rb
@@ -4,6 +4,7 @@
       generator.assets false
       generator.controller_specs false
       generator.helper false
+      generator.template_engine :slim
       generator.test_framework :rspec
       generator.view_specs false
       generator.orm :active_record, primary_key_type: :uuid
